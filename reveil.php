63e697e820d425035c8dee0c27ab3a83c8e11737
dylan-olivro <dylan.olivro@laplateforme.io>
2023-04-21 09:37:48 +0200

    ajout du css

--- reveil.php
+++ reveil.php
@@ -15,24 +15,36 @@
 
 <body>
     <?php require_once('./include/header.php'); ?>
-    <h1>Réveil</h1>
-    <label for="alarm-time">Heure :</label>
-    <input type="time" id="alarm-time">
-    <br>
-    <label for="alarm-day">Jour :</label>
-    <input type="date" id="alarm-day">
-    <br>
-    <label for="alarm-message">Message :</label>
-    <input type="text" id="alarm-message">
-    <br>
-    <button onclick="setAlarm()">Définir l'alarme</button>
+    <main>
+        <div class="input">
+            <!-- <label for="alarm-time">Heure :</label> -->
+            <input type="time" id="alarm-time">
+            <!-- <br> -->
+            <!-- <label for="alarm-day">Jour :</label> -->
+            <input type="date" id="alarm-day">
+        </div>
+        <!-- <br> -->
+        <!-- <label for="alarm-message">Message :</label> -->
+        <div class="messageDiv">
+            <input type="text" id="alarm-message" class="message" placeholder="Description">
+        </div>
+        <!-- <br> -->
+        <div class="btnDiv">
+            <button class="btn" onclick="setAlarm()">Définir l'alarme</button>
+        </div>
 
-    <h2>Alarmes à venir</h2>
-    <ul id="upcoming-alarms-list"></ul>
+        <div class="list">
+            <div class="upcoming">
+                <h2>Alarmes à venir</h2>
+                <ul id="upcoming-alarms-list"></ul>
+            </div>
+            <div class="passed">
+                <h2>Alarmes passées</h2>
+                <ul id="passed-alarms-list"></ul>
+            </div>
+        </div>
 
-    <h2>Alarmes passées</h2>
-    <ul id="passed-alarms-list"></ul>
-
+    </main>
 
 </body>
 
