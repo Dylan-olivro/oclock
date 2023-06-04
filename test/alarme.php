593e673c04f1362ddb072f0ce89421bc81307504
dylan-olivro <dylan.olivro@laplateforme.io>
2023-06-04 18:26:02 +0200

    add sql

--- test/alarme.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('./include/bd.php');
-$recupAlarm = $bdd->prepare("SELECT * from alarme");
-$recupAlarm->execute();
-$result =  $recupAlarm->fetchAll(PDO::FETCH_ASSOC);
-$json = json_encode($result);
-echo $json;
